39ecbadb0ea24425028fae39ac7d944e1e95f786
RamakrishnaBoppana <40335590+RamakrishnaBoppana@users.noreply.github.com>
2018-08-12 15:05:42 +0530

    easyBanking First commit.

---
+++ easyBanking-Security/src/main/java/com/easybanking/security/EasyBankingSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.easybanking.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBankingSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyBankingSecurityApplication.class, args);
+	}
+}
